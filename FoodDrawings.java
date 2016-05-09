d3418a4a4ffd16a7f0bb185f12a27e5d48c48add
ypatel8 <ypatel8@students.d125.org>
2016-05-09 08:48:55 -0500

    Pizza and Drink are up, still need to add the coordinate variables

--- FoodDrawings.java
+++ FoodDrawings.java
@@ -7,10 +7,26 @@ public class FoodDrawings extends Applet
 	public void paint(Graphics g)
 	{
 		//Background
-		setBackground(Color.BLACK);
+		setBackground(Color.gray);
 		
 		this.pizza(g);
+		this.drink(g);
 	}
 	public void pizza(Graphics g)
-	{P
+	{
+		Color bread=new Color(244,164,96);
+		Color cheese=Color.white;
+		g.setColor(bread);
+		g.fillOval(200, 200, 200, 40);
+		g.setColor(cheese);
+		g.fillOval(215, 205, 170, 28);
+	}
+	public void drink(Graphics g)
+	{
+		Color cup=Color.RED;
+		Color straw=Color.white;
+		g.setColor(cup);
+		g.fillRect(100, 100, 50, 100);
+		g.setColor(straw);
+	}
 }
