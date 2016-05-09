1bd043f04d43c255b8ea8cf1ea5fa019b4cd9458
ieraydursun <eray@ireigntech.com>
2016-05-09 08:27:19 -0500

    Added "add" and "getOrder" methods

--- Order.java
+++ Order.java
@@ -1,4 +1,15 @@
 
-public class Order { 
-
+public class Order {
+	private List<Food> theOrder= new ArrayList<Food>();
+	public Order(){
+		
+	}
+	
+	public void add(Food item){
+		theOrder.add(item);
+	}
+	
+	public ArrayList<Food> getOrder(){
+		return theOrder;
+	}
 }
