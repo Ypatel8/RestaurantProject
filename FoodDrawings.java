4d28192be86d5d23fba0fb1ccaa80ae2551e3ae5
ypatel8 <ypatel8@students.d125.org>
2016-05-13 08:59:00 -0500

    buttons and listeners added

--- FoodDrawings.java
+++ FoodDrawings.java
@@ -1,52 +1,188 @@
-import java.awt.Graphics;
-import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.awt.Graphics.*;
 import java.awt.Color;
-
-public class FoodDrawings extends Applet
+public class FoodDrawings
 {
-    public void paint(Graphics g)
-    {
-        //Background
-        setBackground(Color.gray);
-                
-        this.pizza(g);
-        this.drink(g);
+    private JFrame frame;
+    private JPanel panel;
+    private int nP,nB,nSand,nSoda,nW,nJ;
+    private Graphics x;
+    private JLabel pizza,burger,sandwich,soda,water,juice;
+    private JButton numPizza,numBurger,numSandwich,numSoda,numWater,numJuice,orderUpButton;
+    public FoodDrawings() {
+        panel=new JPanel();
+        panel.setPreferredSize(new Dimension(500,500));
+        panel.setBackground(Color.red);
+        
+        frame=new JFrame("Game");
+        
+        pizza = new JLabel("Num of Pizzas");
+        //pizza.setBounds(29, 11, 90, 16);
+        panel.add(pizza);
+        
+        numPizza = new JButton("Add Pizza");
+        //numPizza.setBounds(124, 5, 37, 28);
+        panel.add(numPizza);
+        numPizza.addActionListener(new PizzaButtonListener());
+
+        burger = new JLabel("Num of Burgers");
+        //burger.setBounds(166, 11, 98, 16);
+        panel.add(burger);
+
+        numBurger = new JButton("Add Burger");
+        //numBurger.setBounds(269, 5, 37, 28);
+        panel.add(numBurger);
+        numBurger.addActionListener(new BurgerButtonListener());
+
+        sandwich = new JLabel("Num of Sandwichs");
+        //sandwich.setBounds(311, 11, 117, 16);
+        panel.add(sandwich);
+
+        numSandwich = new JButton("Add Sandwich");
+        //numSandwich.setBounds(433, 5, 37, 28);
+        panel.add(numSandwich);
+        numSandwich.addActionListener(new SandwichButtonListener());
+        
+        soda=new JLabel("Num of Sodas");
+        //soda.setBounds(54, 44, 88, 16);
+        panel.add(soda);
+
+        numSoda = new JButton("Add Soda");
+        //numSoda.setBounds(147, 38, 37, 28);
+        panel.add(numSoda);
+        numSoda.addActionListener(new SodaButtonListener());
+
+        water = new JLabel("Num of Water");
+        //water.setBounds(189, 44, 86, 16);
+        panel.add(water);
+
+        numWater = new JButton("Add Water");
+        //numWater.setBounds(280, 38, 37, 28);
+        panel.add(numWater);
+        numWater.addActionListener(new WaterButtonListener());
+
+        juice = new JLabel("Num of Juice");
+        //juice.setBounds(322, 44, 81, 16);
+        panel.add(juice);
+
+        numJuice = new JButton("Add Juice");
+        //numJuice.setBounds(408, 38, 37, 28);
+        panel.add(numJuice);
+        numJuice.addActionListener(new JuiceButtonListener());
+
+        orderUpButton = new JButton("Order Up!");
+        //orderUpButton.setBounds(189, 78, 117, 29);
+        panel.add(orderUpButton);
+        orderUpButton.addActionListener(new OrderButtonListener());
+
+        JTextArea orderMenu = new JTextArea();
+        orderMenu.setBackground(Color.ORANGE);
+        orderMenu.setEditable(false);
+        orderMenu.setLineWrap(true);
+        orderMenu.setText("s");
+        orderMenu.setBounds(6, 289, 488, 205);
+        panel.add(orderMenu);
+        
+        frame.getContentPane().add(panel);
+        //pizza(x);
     }
-    public void pizza(Graphics g)
+    
+    public void display()
     {
-        Color bread=new Color(244,164,96);
-        Color cheese=Color.white;
-        g.setColor(bread);
-        g.fillOval(200, 200, 200, 40);
-        g.setColor(cheese);
-        g.fillOval(215, 205, 170, 28);
-        for(int x=0;x<=50;x++)
+         frame.pack();
+         frame.setVisible(true);
+    }
+    
+    public class PizzaButtonListener implements ActionListener
+    {
+        public void actionPerformed(ActionEvent ae)
         {
-            if(x%2==0)
-            {
-                g.setColor(Color.red);
-                g.fillRect((int)(Math.random()*170)+215,(int)(Math.random()*28)+205,5,5);
-            }
-            else
-            {
-                g.setColor(Color.green);
-                g.fillRect((int)(Math.random()*170)+215,(int)(Math.random()*28)+205,5,5);
-            }
+            nP++;
+            pizza.setText("NumPizza= "+nP);
         }
     }
-    public void drink(Graphics g)
+    
+    public class BurgerButtonListener implements ActionListener
     {
-        Color cup=Color.RED;
-        Color straw=Color.white;
-        g.setColor(cup);
-        g.fillRect(100, 100, 50, 100);
-        g.setColor(straw);
-        g.fillRect(120, 80, 10, 20);
+        public void actionPerformed(ActionEvent ae)
+        {
+            nB++;
+            burger.setText("NumBurger= "+nB);
+        }
     }
-    public void sandwich(Graphics g, boolean isBurger)
+    
+    public class SandwichButtonListener implements ActionListener
     {
-        if(isBurger==true)
-        {}}
+        public void actionPerformed(ActionEvent ae)
+        {
+            nSand++;
+            sandwich.setText("NumSandwich= "+nSand);
+        }
+    }
+    
+    public class SodaButtonListener implements ActionListener
+    {
+        public void actionPerformed(ActionEvent ae)
+        {
+            nSoda++;
+            soda.setText("NumSoda= "+nSoda);
+        }
+    }
+    
+    public class WaterButtonListener implements ActionListener
+    {
+        public void actionPerformed(ActionEvent ae)
+        {
+            nW++;
+            water.setText("NumWater= "+nW);
+        }
+    }
+    
+    public class JuiceButtonListener implements ActionListener
+    {
+        public void actionPerformed(ActionEvent ae)
+        {
+            nJ++;
+            juice.setText("NumPizza= "+nJ);
+        }
+    }
+    
+    public class OrderButtonListener implements ActionListener
+    {
+        public void actionPerformed(ActionEvent ae)
+        {
+        }
+    }
             
+// 
+//     public void paint(Graphics g)
+//     {
+//         //Background
+//         setBackground(Color.gray);
+// 
+//         this.pizza(g);
+//         this.drink(g);
+//     }
+// 
+//     public void pizza(Graphics g)
+//     {
+//         Color bread=new Color(244,164,96);
+//         Color cheese=Color.white;
+//         g.setColor(new Color(244,164,96));
+//         g.fillOval(200, 200, 200, 40);
+//         g.setColor(cheese);
+//         g.fillOval(215, 205, 170, 28);
+//     }
+// 
+//     public void drink(Graphics g)
+//     {
+//         Color cup=Color.RED;
+//         Color straw=Color.white;
+//         g.setColor(cup);
+//         g.fillRect(100, 100, 50, 100);
+//         g.setColor(straw);
+//     }
 }
 
---
+++ a copy.java
@@ -0,0 +1,8 @@
+public class a
+{
+    public static void main(String [] args)
+    {
+        FoodDrawings x=new FoodDrawings();
+        x.display();
+    }
+}
