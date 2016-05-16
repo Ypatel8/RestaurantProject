b6a9fd9f311f9e6d8c2fcfd34dd9b1619f0a663a
ieraydursun <eray@ireigntech.com>
2016-05-16 08:14:18 -0500

    Random fixed

--- Runner.java
+++ Runner.java
@@ -3,34 +3,45 @@ public class Runner {
     private double moneyMade = 0.0;
 
     public static void main(String[] args) {
-        orderCreator();
-        orderPrinter(????);
-    }
-
-    public static void orderCreator(){
-        Customer cust = new Customer();
-        Order order = new Order();
-        int random;
+        Order o1 = new Order();
+        Customer cust1 = new Customer();
 
         for(int x=0;x<5;x++){
-            random = (int)(Math.random()*2);
-            if(random==0){
-                order.add(cust.getFoodOrder());
-            }
-            else{
-                order.add(cust.getDrinkOrder());
-            }
+            o1 = new Order();
+            o1.add(cust1.getFoodOrder());
+            o1.add(cust1.getDrinkOrder());
+            System.out.println("Order Up");
+            orderPrinter(o1);
         }
+        orderPrinter(o1);
     }
 
-    public static void orderPrinter(List<Food> x){
-        while(x.size()>0){
-            System.out.println(x.remove(0));
-            if(x.size()>0){
+    /* public static void orderCreator(){
+    Customer cust = new Customer();
+    Order order = new Order();
+    int random;
+
+    for(int x=0;x<5;x++){
+    random = (int)(Math.random()*2);
+    if(random==0){
+    order.add(cust.getFoodOrder());
+    }
+    else{
+    order.add(cust.getDrinkOrder());
+    }
+    }
+    }*/
+
+    public static void orderPrinter(Order x){
+        List<Food> orderList = x.getOrder();
+        System.out.println();
+        while(orderList.size()>0){
+            System.out.println(orderList.remove(0));
+            if(orderList.size()>0){
                 orderPrinter(x);
             }
             else{
-
+                System.out.println();
             }
         }
     }
