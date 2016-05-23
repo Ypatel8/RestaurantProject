46bd0847143940c6e8d50083ffea952d7f0f3871
ypatel8 <ypatel8@students.d125.org>
2016-05-23 08:46:44 -0500

    Stopwatch waiting integration

--- Stopwatch.java
+++ Stopwatch.java
@@ -3,30 +3,47 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class Stopwatch {
-static int interval;
-static Timer timer;
-
-public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.print("Input seconds => : ");
-    String secs = sc.nextLine();
-    int delay = 1000;
-    int period = 1000;
-    timer = new Timer();
-    interval = Integer.parseInt(secs);
-    System.out.println(secs);
-    timer.scheduleAtFixedRate(new TimerTask() {
-
-        public void run() {
-            System.out.println(setInterval());
-
+    private static int interval;
+    private static Timer timer;
+    private static int easy=50;
+    private static int medium=30;
+    private static int hard=25;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input difficulty => : ");
+        String secs = sc.nextLine();
+        if(secs.equalsIgnoreCase("easy"))
+        {
+            interval=easy;
         }
-    }, delay, period);
-}
+        else if(secs.equalsIgnoreCase("medium"))
+        {
+            interval=medium;
+        }
+        else if(secs.equalsIgnoreCase("hard"))
+        {
+            interval=hard;
+        }
+        else
+        {
+            interval=medium;
+        }
+        int delay = 1000;
+        int period = 1000;
+        timer = new Timer();
+        System.out.println(secs);
+        timer.scheduleAtFixedRate(new TimerTask() {
 
-private static final int setInterval() {
-    if (interval == 1)
-        timer.cancel();
-    return --interval;
-}
+                public void run() {
+                    System.out.println(setInterval());
+
+                }
+            }, delay, period);
+    }
+
+    private static final int setInterval() {
+        if (interval == 1)
+            timer.cancel();
+        return --interval;
+    }
 }
